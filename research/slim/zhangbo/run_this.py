307a355173240a275d7c4bc41c15a50e9d1da35b
zhangbo <dazhangbo_01@163.com>
2019-03-29 00:56:39 +0800

    all

--- research/slim/zhangbo/run_this.py
+++ research/slim/zhangbo/run_this.py
@@ -68,21 +68,41 @@ def write_into_tfrecord(file_path_in, tf_writer):
                 example = image_to_tfexample(
                     png_string, b'png', _IMAGE_SIZE, _IMAGE_SIZE, label)
                 tf_writer.write(example.SerializeToString())
+
+def write_into_tfrecord_v2(file_path_in, tf_writer):
+    with tf.gfile.Open(file_path_in, 'rb') as f:
+        if sys.version_info < (3,):
+            data = cPickle.load(f)  # python2
+        else:
+            data = cPickle.load(f, encoding='bytes')  # python3
+    images = data[b'data']
+    num_images = images.shape[0]
+    images = images.reshape((num_images, 3, 32, 32))
+    labels = data[b'labels']
+    for j in range(num_images):
+        image = np.squeeze(images[j]).transpose((1, 2, 0))
+        label = labels[j]
+        image_raw = image.tostring()
+        example = image_to_tfexample(
+            image_raw, b'png', _IMAGE_SIZE, _IMAGE_SIZE, label)
+        tf_writer.write(example.SerializeToString())
+
+
 def cifar_to_tfrecord():
     input_dir = FLAGS.input_dir
     train_file_names = ["data_batch_"+str(i) for i in range(1, 6, 1)]
     test_file_names = ["test_batch"]
-    file_path_train_out = os.path.join(input_dir, "train.tfrecord")
+    file_path_train_out = os.path.join(input_dir, "train_v2.tfrecord")
     tf_writer = tf.python_io.TFRecordWriter(file_path_train_out)
     for index, name in enumerate(train_file_names):
         file_path_in = os.path.join(input_dir, name)
-        write_into_tfrecord(file_path_in, tf_writer)
+        write_into_tfrecord_v2(file_path_in, tf_writer)
     tf_writer.close()
-    file_path_test_out = os.path.join(input_dir, "test.tfrecord")
+    file_path_test_out = os.path.join(input_dir, "test_v2.tfrecord")
     tf_writer_test = tf.python_io.TFRecordWriter(file_path_test_out)
     for index, name in enumerate(test_file_names):
         file_path_in = os.path.join(input_dir, name)
-        write_into_tfrecord(file_path_in, tf_writer_test)
+        write_into_tfrecord_v2(file_path_in, tf_writer_test)
     tf_writer_test.close()
 
 
